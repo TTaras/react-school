e334fbe60ec079681cd9ac640295ce70f450cef3
Taras Tarasov <t.tarasov@rbc.ru>
2021-04-07 19:24:00 +0300

    lesson 5 - fix by comments

--- src/store/messages/reducer.js
+++ src/store/messages/reducer.js
@@ -5,19 +5,17 @@ const initialState = {};
 export const messagesReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_MESSAGE: {
-      const newState = Object.assign({}, state);
-      const messages = newState[action.chatId] || [];
+      const messages = state[action.chatId] || [];
       const newId = `id_${messages.length + 1}`;
 
-      messages.push({
-        id: newId,
-        author: action.author,
-        text: action.text,
-      });
-
-      newState[action.chatId] = messages;
-
-      return newState;
+      return {
+        ...state,
+        [action.chatId]: messages.concat({
+          id: newId,
+          author: action.author,
+          text: action.text,
+        })
+      }
     }
 
     default:
--- src/store/profile/reducer.js
+++ src/store/profile/reducer.js
@@ -8,9 +8,8 @@ export const profileReducer = (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_NAME: {
       return {
-        ...state, ...{
-          name: action.name
-        }
+        ...state,
+        name: action.name
       };
     }
     default:
